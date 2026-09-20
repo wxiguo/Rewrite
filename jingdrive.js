61214d9d5d58966d19f01a58fb438eddaaa1a4cc
WangXiguo <wxq9375@gmail.com>
2026-09-20 17:28:22 +0800

    update

--- jingdrive.js
+++ jingdrive.js
@@ -14,136 +14,284 @@ hostname = api.jingdrive.cn
 *************************************/
 
 // ==================== 1. 轻量级 AES 加密模块 ====================
-const AES = (function() {
-    const Sbox = [
-        99,124,119,123,242,107,111,197,48,1,103,43,254,215,171,118,202,130,201,125,250,89,71,240,173,212,162,175,
-        156,164,114,192,183,253,147,38,54,63,247,204,52,165,229,241,113,216,49,21,4,199,35,195,24,150,5,154,7,
-        18,128,226,235,39,178,117,9,131,44,26,27,110,90,160,82,59,214,179,41,227,47,132,83,209,0,237,32,252,177,
-        91,106,203,190,57,74,76,88,207,208,239,170,85,75,163,169,149,161,244,50,22,191,186,62,10,198,242,94,221,
-        6,157,247,213,172,98,145,149,228,121,231,200,55,109,141,213,78,169,108,86,244,234,101,122,174,8,186,120,
-        37,46,28,166,180,198,232,221,116,31,75,189,139,138,112,60,81,168,148,245,223,36,92,180,211,40,17,102,143,
-        86,43,42,215,26,197,62,94,251,240,161,224,125,23,172,130,214,142,226,205,188,143,110,134,53,101,146,15,
-        159,84,93,12,137,13,236,97,34,167,224,72,248,9,249,158,111,245,67,115,117,151,194,2,243,153,196,165,104,
-        140,210,115,126,170,163,70,16,137,79,40,208,134,124,182,145,218,70,92,127,136,101,29,152,248,206,255,243,
-        228,60,158,144,147,193,168,80,64,100,61,185,178,133,157,249,240,220,107,219,87,14,39,81,171,77,225,90,
-        217,162,155,17,211,255,207,229,66,74,244,88,219,130,202,56,80,35,127,24,180,44,214,196,17,212,83,45
+// ==================== AES-128-CBC / PKCS#7 ====================
+const AES = (() => {
+
+    // 标准 AES S-Box，必须正好 256 个元素
+    const SBOX = [
+        0x63,0x7c,0x77,0x7b,0xf2,0x6b,0x6f,0xc5,0x30,0x01,0x67,0x2b,0xfe,0xd7,0xab,0x76,
+        0xca,0x82,0xc9,0x7d,0xfa,0x59,0x47,0xf0,0xad,0xd4,0xa2,0xaf,0x9c,0xa4,0x72,0xc0,
+        0xb7,0xfd,0x93,0x26,0x36,0x3f,0xf7,0xcc,0x34,0xa5,0xe5,0xf1,0x71,0xd8,0x31,0x15,
+        0x04,0xc7,0x23,0xc3,0x18,0x96,0x05,0x9a,0x07,0x12,0x80,0xe2,0xeb,0x27,0xb2,0x75,
+        0x09,0x83,0x2c,0x1a,0x1b,0x6e,0x5a,0xa0,0x52,0x3b,0xd6,0xb3,0x29,0xe3,0x2f,0x84,
+        0x53,0xd1,0x00,0xed,0x20,0xfc,0xb1,0x5b,0x6a,0xcb,0xbe,0x39,0x4a,0x4c,0x58,0xcf,
+        0xd0,0xef,0xaa,0xfb,0x43,0x4d,0x33,0x85,0x45,0xf9,0x02,0x7f,0x50,0x3c,0x9f,0xa8,
+        0x51,0xa3,0x40,0x8f,0x92,0x9d,0x38,0xf5,0xbc,0xb6,0xda,0x21,0x10,0xff,0xf3,0xd2,
+        0xcd,0x0c,0x13,0xec,0x5f,0x97,0x44,0x17,0xc4,0xa7,0x7e,0x3d,0x64,0x5d,0x19,0x73,
+        0x60,0x81,0x4f,0xdc,0x22,0x2a,0x90,0x88,0x46,0xee,0xb8,0x14,0xde,0x5e,0x0b,0xdb,
+        0xe0,0x32,0x3a,0x0a,0x49,0x06,0x24,0x5c,0xc2,0xd3,0xac,0x62,0x91,0x95,0xe4,0x79,
+        0xe7,0xc8,0x37,0x6d,0x8d,0xd5,0x4e,0xa9,0x6c,0x56,0xf4,0xea,0x65,0x7a,0xae,0x08,
+        0xba,0x78,0x25,0x2e,0x1c,0xa6,0xb4,0xc6,0xe8,0xdd,0x74,0x1f,0x4b,0xbd,0x8b,0x8a,
+        0x70,0x3e,0xb5,0x66,0x48,0x03,0xf6,0x0e,0x61,0x35,0x57,0xb9,0x86,0xc1,0x1d,0x9e,
+        0xe1,0xf8,0x98,0x11,0x69,0xd9,0x8e,0x94,0x9b,0x1e,0x87,0xe9,0xce,0x55,0x28,0xdf,
+        0x8c,0xa1,0x89,0x0d,0xbf,0xe6,0x42,0x68,0x41,0x99,0x2d,0x0f,0xb0,0x54,0xbb,0x16
     ];
-    const Rcon = [0, 1, 2, 4, 8, 16, 32, 64, 128, 27, 54];
 
-    function toUtf8Bytes(str) {
-        let bytes = [];
-        for (let i = 0; i < str.length; i++) {
-            let code = str.charCodeAt(i);
-            if (code < 0x80) bytes.push(code);
-            else if (code < 0x800) bytes.push(0xc0 | (code >> 6), 0x80 | (code & 0x3f));
-            else if (code < 0xd800 || code >= 0xe000) {
-                bytes.push(0xe0 | (code >> 12), 0x80 | ((code >> 6) & 0x3f), 0x80 | (code & 0x3f));
+    const RCON = [
+        0x00,0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80,0x1b,0x36
+    ];
+
+    function utf8(str) {
+        const out = [];
+
+        for (const ch of str) {
+            const cp = ch.codePointAt(0);
+
+            if (cp <= 0x7f) {
+                out.push(cp);
+            } else if (cp <= 0x7ff) {
+                out.push(
+                    0xc0 | (cp >> 6),
+                    0x80 | (cp & 0x3f)
+                );
+            } else if (cp <= 0xffff) {
+                out.push(
+                    0xe0 | (cp >> 12),
+                    0x80 | ((cp >> 6) & 0x3f),
+                    0x80 | (cp & 0x3f)
+                );
+            } else {
+                out.push(
+                    0xf0 | (cp >> 18),
+                    0x80 | ((cp >> 12) & 0x3f),
+                    0x80 | ((cp >> 6) & 0x3f),
+                    0x80 | (cp & 0x3f)
+                );
             }
         }
-        return bytes;
+
+        return out;
     }
 
-    function toBase64(bytes) {
-        const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
-        let res = '';
+    function base64(bytes) {
+        const chars =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+        let result = "";
+
         for (let i = 0; i < bytes.length; i += 3) {
-            let b1 = bytes[i], b2 = bytes[i + 1], b3 = bytes[i + 2];
-            res += chars[b1 >> 2];
-            res += chars[((b1 & 3) << 4) | ((b2 || 0) >> 4)];
-            res += b2 !== undefined ? chars[((b2 & 15) << 2) | ((b3 || 0) >> 6)] : '=';
-            res += b3 !== undefined ? chars[b3 & 63] : '=';
+            const a = bytes[i];
+            const b = i + 1 < bytes.length ? bytes[i + 1] : 0;
+            const c = i + 2 < bytes.length ? bytes[i + 2] : 0;
+
+            result += chars[a >> 2];
+            result += chars[((a & 3) << 4) | (b >> 4)];
+            result += i + 1 < bytes.length
+                ? chars[((b & 15) << 2) | (c >> 6)]
+                : "=";
+            result += i + 2 < bytes.length
+                ? chars[c & 63]
+                : "=";
         }
-        return res;
+
+        return result;
     }
 
-    function keyExpansion(keyBytes) {
-        const Nk = keyBytes.length / 4;
-        const Nr = Nk + 6;
-        let w = new Uint32Array(4 * (Nr + 1));
-        for (let i = 0; i < Nk; i++) {
-            w[i] = (keyBytes[4 * i] << 24) | (keyBytes[4 * i + 1] << 16) | (keyBytes[4 * i + 2] << 8) | keyBytes[4 * i + 3];
-        }
-        for (let i = Nk; i < 4 * (Nr + 1); i++) {
-            let temp = w[i - 1];
-            if (i % Nk === 0) {
-                temp = ((temp << 8) | (temp >>> 24));
-                temp = (Sbox[(temp >>> 24) & 0xff] << 24) | (Sbox[(temp >>> 16) & 0xff] << 16) | (Sbox[(temp >>> 8) & 0xff] << 8) | Sbox[temp & 0xff];
-                temp ^= (Rcon[i / Nk] << 24);
-            }
-            w[i] = w[i - Nk] ^ temp;
-        }
-        return { w, Nr };
+    function xtime(x) {
+        return ((x << 1) ^ ((x & 0x80) ? 0x1b : 0)) & 0xff;
     }
 
-    function encryptBlock(pt, w, Nr) {
-        let s = new Uint8Array(16);
-        for (let i = 0; i < 16; i++) s[i] = pt[i] ^ ((w[Math.floor(i / 4)] >>> (24 - 8 * (i % 4))) & 0xff);
-        for (let round = 1; round < Nr; round++) {
-            let t = new Uint8Array(16);
-            for (let i = 0; i < 16; i++) t[i] = Sbox[s[i]];
-            s[0] = t[0]; s[4] = t[4]; s[8] = t[8]; s[12] = t[12];
-            s[1] = t[5]; s[5] = t[9]; s[9] = t[13]; s[13] = t[1];
-            s[2] = t[10]; s[6] = t[14]; s[10] = t[2]; s[14] = t[6];
-            s[3] = t[15]; s[7] = t[3]; s[11] = t[7]; s[15] = t[11];
+    function expandKey(key) {
+        if (key.length !== 16) {
+            throw new Error("AES-128 key must be 16 bytes");
+        }
 
-            for (let c = 0; c < 4; c++) {
-                let a = s[4 * c], b = s[4 * c + 1], c_ = s[4 * c + 2], d = s[4 * c + 3];
-                let h = (x) => ((x << 1) ^ (((x >> 7) & 1) * 0x11b)) & 0xff;
-                s[4 * c]     = h(a) ^ (h(b) ^ b) ^ c_ ^ d;
-                s[4 * c + 1] = a ^ h(b) ^ (h(c_) ^ c_) ^ d;
-                s[4 * c + 2] = a ^ b ^ h(c_) ^ (h(d) ^ d);
-                s[4 * c + 3] = (h(a) ^ a) ^ b ^ c_ ^ h(d);
-            }
+        const w = new Uint8Array(176);
 
-            let rw = w[4 * round], rw1 = w[4 * round + 1], rw2 = w[4 * round + 2], rw3 = w[4 * round + 3];
+        for (let i = 0; i < 16; i++) {
+            w[i] = key[i];
+        }
+
+        let bytesGenerated = 16;
+        let rconIndex = 1;
+        const temp = new Uint8Array(4);
+
+        while (bytesGenerated < 176) {
+
             for (let i = 0; i < 4; i++) {
-                s[i] ^= (rw >>> (24 - 8 * i)) & 0xff;
-                s[4 + i] ^= (rw1 >>> (24 - 8 * i)) & 0xff;
-                s[8 + i] ^= (rw2 >>> (24 - 8 * i)) & 0xff;
-                s[12 + i] ^= (rw3 >>> (24 - 8 * i)) & 0xff;
+                temp[i] = w[bytesGenerated - 4 + i];
+            }
+
+            if (bytesGenerated % 16 === 0) {
+
+                const t = temp[0];
+
+                temp[0] = SBOX[temp[1]];
+                temp[1] = SBOX[temp[2]];
+                temp[2] = SBOX[temp[3]];
+                temp[3] = SBOX[t];
+
+                temp[0] ^= RCON[rconIndex++];
+            }
+
+            for (let i = 0; i < 4; i++) {
+                w[bytesGenerated] =
+                    w[bytesGenerated - 16] ^ temp[i];
+
+                bytesGenerated++;
             }
         }
 
-        let t = new Uint8Array(16);
-        for (let i = 0; i < 16; i++) t[i] = Sbox[s[i]];
-        s[0] = t[0]; s[4] = t[4]; s[8] = t[8]; s[12] = t[12];
-        s[1] = t[5]; s[5] = t[9]; s[9] = t[13]; s[13] = t[1];
-        s[2] = t[10]; s[6] = t[14]; s[10] = t[2]; s[14] = t[6];
-        s[3] = t[15]; s[7] = t[3]; s[11] = t[7]; s[15] = t[11];
+        return w;
+    }
 
-        let fw = w[4 * Nr], fw1 = w[4 * Nr + 1], fw2 = w[4 * Nr + 2], fw3 = w[4 * Nr + 3];
-        for (let i = 0; i < 4; i++) {
-            s[i] ^= (fw >>> (24 - 8 * i)) & 0xff;
-            s[4 + i] ^= (fw1 >>> (24 - 8 * i)) & 0xff;
-            s[8 + i] ^= (fw2 >>> (24 - 8 * i)) & 0xff;
-            s[12 + i] ^= (fw3 >>> (24 - 8 * i)) & 0xff;
+    function addRoundKey(state, key, round) {
+        const offset = round * 16;
+
+        for (let i = 0; i < 16; i++) {
+            state[i] ^= key[offset + i];
         }
-        return s;
+    }
+
+    function subBytes(state) {
+        for (let i = 0; i < 16; i++) {
+            state[i] = SBOX[state[i]];
+        }
+    }
+
+    function shiftRows(state) {
+        const t = state.slice();
+
+        state[0]  = t[0];
+        state[4]  = t[4];
+        state[8]  = t[8];
+        state[12] = t[12];
+
+        state[1]  = t[5];
+        state[5]  = t[9];
+        state[9]  = t[13];
+        state[13] = t[1];
+
+        state[2]  = t[10];
+        state[6]  = t[14];
+        state[10] = t[2];
+        state[14] = t[6];
+
+        state[3]  = t[15];
+        state[7]  = t[3];
+        state[11] = t[7];
+        state[15] = t[11];
+    }
+
+    function mixColumns(state) {
+
+        for (let c = 0; c < 4; c++) {
+
+            const i = c * 4;
+
+            const a = state[i];
+            const b = state[i + 1];
+            const c2 = state[i + 2];
+            const d = state[i + 3];
+
+            state[i] =
+                xtime(a) ^
+                (xtime(b) ^ b) ^
+                c2 ^
+                d;
+
+            state[i + 1] =
+                a ^
+                xtime(b) ^
+                (xtime(c2) ^ c2) ^
+                d;
+
+            state[i + 2] =
+                a ^
+                b ^
+                xtime(c2) ^
+                (xtime(d) ^ d);
+
+            state[i + 3] =
+                (xtime(a) ^ a) ^
+                b ^
+                c2 ^
+                xtime(d);
+        }
+    }
+
+    function encryptBlock(input, roundKeys) {
+
+        const state = new Uint8Array(input);
+
+        addRoundKey(state, roundKeys, 0);
+
+        for (let round = 1; round <= 10; round++) {
+
+            subBytes(state);
+            shiftRows(state);
+
+            if (round !== 10) {
+                mixColumns(state);
+            }
+
+            addRoundKey(state, roundKeys, round);
+        }
+
+        return state;
+    }
+
+    function encryptCBC(plainText, keyText, ivText) {
+
+        const data = utf8(plainText);
+        const key = utf8(keyText);
+        const iv = utf8(ivText);
+
+        if (key.length !== 16) {
+            throw new Error("Key must be 16 bytes");
+        }
+
+        if (iv.length !== 16) {
+            throw new Error("IV must be 16 bytes");
+        }
+
+        // PKCS#7
+        const pad = 16 - (data.length % 16);
+
+        for (let i = 0; i < pad; i++) {
+            data.push(pad);
+        }
+
+        const roundKeys = expandKey(key);
+
+        let previous = iv.slice();
+        const output = [];
+
+        for (let offset = 0; offset < data.length; offset += 16) {
+
+            const block = new Uint8Array(16);
+
+            for (let i = 0; i < 16; i++) {
+                block[i] =
+                    data[offset + i] ^
+                    previous[i];
+            }
+
+            const encrypted =
+                encryptBlock(block, roundKeys);
+
+            for (let i = 0; i < 16; i++) {
+                output.push(encrypted[i]);
+            }
+
+            previous = Array.from(encrypted);
+        }
+
+        return base64(output);
     }
 
     return {
-        encryptCBC: function(plainText, keyStr, ivStr) {
-            let key = toUtf8Bytes(keyStr);
-            let iv = toUtf8Bytes(ivStr);
-            let data = toUtf8Bytes(plainText);
-
-            let padLen = 16 - (data.length % 16);
-            for (let i = 0; i < padLen; i++) data.push(padLen);
-
-            let { w, Nr } = keyExpansion(key);
-            let out = [];
-            let prev = iv.slice(0, 16);
-
-            for (let i = 0; i < data.length; i += 16) {
-                let block = data.slice(i, i + 16);
-                for (let j = 0; j < 16; j++) block[j] ^= prev[j];
-                let encrypted = encryptBlock(block, w, Nr);
-                for (let j = 0; j < 16; j++) out.push(encrypted[j]);
-                prev = encrypted;
-            }
-
-            return toBase64(out);
-        }
+        encryptCBC
     };
 })();
 
